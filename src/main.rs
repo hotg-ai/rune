bc1ab9bee8149ca4b534493844964a3378357ac7
Geert Trooskens <geert@bioskoop.eu>
2020-10-01 14:10:07 +0200

    simple parser implementation

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,14 @@
 
-
 use structopt::StructOpt;
+use pest::iterators::Pair;
+use pest::Parser as P;
+use pest_derive::Parser;
+use std::fs;
+
+#[allow(dead_code)]
+#[derive(Parser)]
+#[grammar = "grammar.pest"]
+struct Parser;
 
 #[derive(Debug,StructOpt)]
 enum Rune {
@@ -19,8 +27,43 @@ enum Rune {
 }
 
 fn main() {
-
-
     let args = Rune::from_args();
-    println!("{:?}", args);
+    match args {
+        Rune::Build { file } => {
+            build(file);
+        }
+        Rune::Exec { container } => {
+            println!("Exec {:?}", container);
+        }
+        Rune::Containers { subcommand } => {
+            println!("Container {:?}", subcommand);
+        }
+    }
+}
+
+fn build(file: std::path::PathBuf) {
+    println!("Building {:?}", file);
+    let contents:&str = &fs::read_to_string(file)
+        .expect("Something went wrong reading the file");
+    parse(contents);
+}
+
+fn parse(file_string:&str) {
+    let pairs = Parser::parse(Rule::ident_list, file_string).unwrap_or_else(|e| panic!("{}", e));
+
+    for pair in pairs {
+        // A pair is a combination of the rule which matched and a span of input
+        println!("Rule:    {:?}", pair.as_rule());
+        println!("Span:    {:?}", pair.as_span());
+        println!("Text:    {}", pair.as_str());
+
+        // A pair can be converted to an iterator of the tokens which make it up:
+        for inner_pair in pair.into_inner() {
+            match inner_pair.as_rule() {
+                Rule::alpha => println!("Letter:  {}", inner_pair.as_str()),
+                Rule::digit => println!("Digit:   {}", inner_pair.as_str()),
+                _ => unreachable!()
+            };
+        }
+    }
 }
