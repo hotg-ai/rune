2cc325307b9e11476051998802fb3c8a33db7eee
Kartik Thakore <kthakore@alumni.uwo.ca>
2020-11-01 10:48:42 -0800

    Cleaning up subcomands

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,47 @@
 
 
 
-use structopt::StructOpt;
-mod rune;
-
-// We should move this to the lib `runefile_parser`
-
-
-
-mod cli;
 mod build;
+mod run;
+
+use log;
+use env_logger;
+
+
+use clap::{App, Arg, SubCommand};
+
+const VERSION: &str = "v0.0.1";
 
 fn main() {
-
-    // Process the cli command
-    let opt = cli::Opts::from_args();
-    handle_subcommand(opt);
+    env_logger::init();
     
-}
+    // Process the cli command
+    let matches = App::new("rune")
+    .version(VERSION)
+    .about("A self-sufficient runtime for TinyML Containers")
+    .subcommands(vec![
+        SubCommand::with_name("build")
+            .arg(Arg::with_name("runefile")
+                .value_name("FILE")
+                .default_value("Runefile")
+                .help("Runefile to build")
+                .takes_value(true)),
+        SubCommand::with_name("run")
+                .arg(Arg::with_name("rune")
+                    .value_name("FILE")
+                    .default_value("sine.rune")
+                    .help("*.rune file to run")
+                    .takes_value(true))
+    ]).get_matches();
 
-
-fn handle_subcommand(opt: cli::Opts){
-  
-    if let Some(subcommand) = opt.commands{
-        match subcommand {
-            cli::Rune::Build(cfg) => {
-
-                build::build(cfg);
-            },
-            cli::Rune::Containers(cfg) => {
-                println!("Containers {:?}", cfg);
-            },
-            cli::Rune::Run(cfg) => {
-                println!("Run {:?}", cfg);
-            },
+    
+    if let Some(matches) = matches.subcommand_matches("build") {
+        match matches.value_of("runefile")  {
+            Some(x) => build::build(x),
+            _ => log::info!("No runefile provided")
 
         }
     }
+    
 }
+
--- src/rune/mod.rs
+++
@@ -1,5 +0,0 @@
-
-
-pub fn hello() {
-    println!("Hello");
-}
