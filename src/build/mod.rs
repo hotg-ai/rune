cbdef9156c4d7d584aec647505e2988202b9ed1d
Kartik Thakore <kthakore@alumni.uwo.ca>
2020-11-21 20:36:00 -0800

    Making building work :D! MEGA HACK!!!

--- src/build/mod.rs
+++ src/build/mod.rs
@@ -1,6 +1,5 @@
 use log;
 use std::fs;
-use clap::ArgMatches;
 
 pub fn build(fileloc: &str) {
     let contents = fs::read_to_string(fileloc);
@@ -22,8 +21,13 @@ pub fn build(fileloc: &str) {
             return;
         }
     };
+    
+
     let mut manifest_path = homedir.clone(); 
     manifest_path.push("Cargo.toml");
+
+    let mut rune_file = homedir.clone();
+    rune_file.push("target/wasm32-unknown-unknown/release/rune.wasm");
 
     let ws = match cargo::core::Workspace::new(&manifest_path, &config) {
         Ok(w) => w,
@@ -33,7 +37,9 @@ pub fn build(fileloc: &str) {
         }
     };
 
-    let compile_opts = match cargo::ops::CompileOptions::new(
+    
+
+    let mut compile_opts = match cargo::ops::CompileOptions::new(
         &cargo::Config::new(cargo::core::Shell::default(), homedir.clone(), homedir),
         cargo::core::compiler::CompileMode::Build // we should also do a test before
     )  {
@@ -44,7 +50,28 @@ pub fn build(fileloc: &str) {
         }
     };
 
-    println!("{:?}", compile_opts);
+    compile_opts.build_config = cargo::core::compiler::BuildConfig::new(
+            &config, 
+            None, 
+            &[String::from("wasm32-unknown-unknown")],
+            cargo::core::compiler::CompileMode::Build
+        ).unwrap();
+
+    compile_opts.spec = cargo::ops::Packages::Default; 
+    compile_opts.filter = cargo::ops::CompileFilter::Default{
+        required_features_filterable: true,
+    };
+    
+    
+    //  * MEGA HUNT DOWN IN THE CARGO CODE ... this is what I had to do
+    //  
+    //    cd /Users/kthakore/Documents/HOTG-ai/cargo; cargo build --quiet; cd /Users/kthakore/.rune/runes/b39f1b98-dbe4-4c2d-98b6-06f544647d4b; echo "BUILD CARGO"; cargo clean; /Users/kthakore/Documents/HOTG-ai/cargo/target/release/cargo build --target wasm32-unknown-unknown --release > DEV_LOG; cd /Users/kthakore/Documents/HOTG-ai/cargo;
+    //    cd /Users/kthakore/Documents/HOTG-ai/cargo; cargo build --release --quiet; cd /Users/kthakore/.rune/runes/b39f1b98-dbe4-4c2d-98b6-06f544647d4b; echo "BUILD CARGO"; cargo clean; /Users/kthakore/Documents/HOTG-ai/cargo/target/release/cargo build --target wasm32-unknown-unknown  > DEV_LOG; cd /Users/kthakore/Documents/HOTG-ai/cargo;
+
+    //    Then I DIFFED that to find out that --release tag was going to profiles!!! No DOCS ANYWHERE!
+
+    compile_opts.build_config.requested_profile = cargo::util::interning::InternedString::new("release");
+   
     match cargo::ops::compile(&ws, &compile_opts) {
         Ok(_x) => log::info!("Rune compiled"),
         Err(err) => {
@@ -52,5 +79,9 @@ pub fn build(fileloc: &str) {
             return;
         }
     }
+
+    log::info!("RUNE MADE AT {:?}", rune_file);
+
+    // 
     
 }
