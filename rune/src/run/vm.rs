9759e7de0e949e8a4791729c9a1b863035f8eb5c
Kartik Thakore <kthakore@alumni.uwo.ca>
2021-01-18 13:08:57 -0800

    Cleaning up

--- rune/src/run/vm.rs
+++ rune/src/run/vm.rs
@@ -1,6 +1,11 @@
 use log;
 use runic_types::*;
 
+use wasmer_runtime::{func, imports, Array, Ctx, Instance, WasmPtr};
+
+use tflite::ops::builtin::BuiltinOpResolver;
+use tflite::{FlatBufferModel, InterpreterBuilder};
+
 
 /// Rune Executor 
 ///  Executes the Rune and provides the appropriate interfaces
@@ -10,8 +15,16 @@ pub struct VM {
 
 ///
 impl VM {
-    pub fn init(fileloc: &str) -> VM {
-        log::info!("Initializing VM {}", fileloc);
+    pub fn init(filename: &str) -> VM {
+        log::info!("Initializing");
+
+        let rune_bytes = match std::fs::read(filename) {
+            Ok(res) => res,
+            Err(_err) => { 
+                log::error!("Failed to load container {}", filename);
+                std::process::exit(1);
+            }
+        };
 
         return VM{};
     }
