3dc38e733ec5662cd30a021d9f766c8567d51529
Kartik Thakore <kthakore@alumni.uwo.ca>
2021-01-18 16:13:04 -0800

    Able to call

--- rune/src/run/vm.rs
+++ rune/src/run/vm.rs
@@ -35,28 +35,11 @@ impl VM {
 
     pub fn call(&self, input: Vec<u8>) -> Vec<u8> {
         let instance = &self.instance;
-        let get_provider_response_buffer_pointer: Func<(), WasmPtr<u8, Array>> = instance
-            .exports
-            .get("provider_response_ptr")
-            .expect("provider_response_ptr");
+       
+       
+        let call_fn: Func<(i32, i32, i32), i32> = instance.exports.get("_call").unwrap();
     
-        let pr_ptr = get_provider_response_buffer_pointer.call().unwrap();
-    
-        let wasm_instance_memory = instance.context().memory(0);
-        log::debug!("Trying to write provider response");
-        let len = input.len() as u32;
-        let memory_writer = pr_ptr.deref(wasm_instance_memory, 0, len).unwrap();
-    
-        //Refactor THIS
-        let mut idx = 0;
-        for b in input.into_iter() {
-            memory_writer[idx].set(b);
-            idx = idx + 1;
-        }
-    
-        let call_fn: Func<u32, i32> = instance.exports.get("_call").unwrap();
-    
-        let feature_buff_size = call_fn.call(len).expect("failed to _call");
+        let feature_buff_size = call_fn.call(input.len() as i32, 0i32, 0i32).expect("failed to _call");
         log::debug!("Guest::_call() returned {}", feature_buff_size);
     
         let feature_data_buf: Vec<u8> = vec![];
