7c60bb164cca484aa108e274afcb7cc65cc45ef5
Kartik Thakore <kthakore@alumni.uwo.ca>
2021-01-19 13:42:05 -0800

    Calling provider as context data

--- rune/src/run/vm.rs
+++ rune/src/run/vm.rs
@@ -4,33 +4,9 @@ use std::ffi::c_void;
 use std::cell::RefCell;
 use wasmer_runtime::{instantiate, Func, Instance, func, imports, Array, Ctx, WasmPtr};
 use std::sync::{Arc, Mutex};
-#[derive(Debug, Clone)]
-pub struct CapabilityParam {
-    pub value: Vec<u8>,
-    pub value_type: runic_types::PARAM_TYPE
-}
-
-#[derive(Debug, Clone)]
-pub struct CapabilityRequest {
-    pub c_type: runic_types::CAPABILITY,
-    pub params: std::collections::HashMap<String, CapabilityParam>
-}
 
 
-impl CapabilityRequest {
-    pub fn init(c_type: runic_types::CAPABILITY) -> CapabilityRequest {
-        return CapabilityRequest{
-            c_type,
-            params: std::collections::HashMap::new()
-        }
-    }
-    pub fn set_param(&mut self, key: String, value: Vec<u8>, value_type: runic_types::PARAM_TYPE) {
-        self.params.insert(key, CapabilityParam{
-            value,
-            value_type
-        });
-    }
-}
+use crate::run::vm::capability::*;
 
 pub struct Model {
     weights: Vec<u8>
