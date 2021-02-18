d7cf16ff7013263383a0b1b194a1d91f3e04805a
Michael-F-Bryan <michaelfbryan@gmail.com>
2021-02-19 03:09:51 +0800

    Started working on a table for managing names

--- syntax/src/hir.rs
+++ syntax/src/hir.rs
@@ -1,12 +1,13 @@
-use std::collections::HashMap;
+//! The *High-level Internal Representation*.
 
-use crate::ast::Ident;
+use std::collections::HashMap;
 
 #[derive(Debug, Default, Clone, PartialEq)]
 pub struct Rune {
     pub base_image: Option<String>,
     pub labels: HashMap<String, HirId>,
     pub sinks: HashMap<HirId, Sink>,
+    pub names: NameTable,
 }
 
 #[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
@@ -23,4 +24,25 @@ impl HirId {
 #[derive(Debug, Copy, Clone, PartialEq)]
 pub enum Sink {
     Serial,
+}
+
+#[derive(Debug, Default, Clone, PartialEq)]
+pub struct NameTable {
+    name_to_id: HashMap<String, HirId>,
+    id_to_name: HashMap<HirId, String>,
+}
+
+impl NameTable {
+    pub fn add(&mut self, name: &str, id: HirId) {
+        self.name_to_id.insert(name.to_string(), id);
+        self.id_to_name.insert(id, name.to_string());
+    }
+
+    pub fn get_name(&self, id: HirId) -> Option<&str> {
+        self.id_to_name.get(&id).map(|s| s.as_str())
+    }
+
+    pub fn get_id(&self, name: &str) -> Option<HirId> {
+        self.name_to_id.get(name).copied()
+    }
 }
