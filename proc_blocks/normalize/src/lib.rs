2ea85d7a4c4fb3687139d86b26790349f54c1311
Kartik Thakore <kthakore@alumni.uwo.ca>
2021-02-20 14:31:52 -0800

    Adding normalize

--- proc_blocks/normalize/src/lib.rs
+++ proc_blocks/normalize/src/lib.rs
@@ -1,7 +1,51 @@
+use runic_types::{Transform, PipelineContext};
+
+struct Normalize {
+
+}
+
+impl Transform<Vec<f32>> for Normalize {
+    type Output = Vec<f32>;
+
+    fn transform(&mut self,
+        input: Vec<f32>,
+        ctx: &mut PipelineContext) -> Self::Output {
+
+        // let input_size = input.len();
+        let mut output: [f32; 384]= [0.0; 384];
+        
+        let min_value = input.iter().fold(f32::INFINITY, |a, &b| a.min(b));
+        let max_value = input.iter().fold(f32::NEG_INFINITY, |a, &b| a.max(b));
+        let denom = max_value-min_value;
+        if denom == 0.0 {
+          return input;
+        }
+      
+        for i in 0..input.len() {
+            
+            output[i] = (input[i]-min_value)/denom as f32;
+        }
+      
+        return output.to_vec();
+      }
+    
+}
+
+
 #[cfg(test)]
 mod tests {
+    use runic_types::{Transform, PipelineContext};
+    use crate::Normalize;
+
+    use rand::{thread_rng, Rng};
     #[test]
     fn it_works() {
-        assert_eq!(2 + 2, 4);
+        let mut input: [f32; 384] = [0.0;384];
+        thread_rng().fill(&mut input[..]);
+        let mut norm_pb: Normalize = Normalize{}; 
+        let mut pipeline = PipelineContext{};
+        let output = norm_pb.transform(input.to_vec(), &mut pipeline);
+      
+        println!(" {:?}", output);
     }
 }
