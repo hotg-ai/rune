f3fcbe20b5fb1364ecfb638c9473da996a4e7ace
Michael-F-Bryan <michaelfbryan@gmail.com>
2021-02-28 15:32:55 +0800

    Made the labelling proc_block generic over its input type

--- proc_blocks/ohv_label/src/lib.rs
+++ proc_blocks/ohv_label/src/lib.rs
@@ -8,16 +8,45 @@ extern crate std;
 use alloc::vec::Vec;
 use runic_types::Transform;
 
-struct OhvLabel {
+pub struct OhvLabel {
     labels: Vec<&'static str>,
     unknown_label: &'static str,
 }
 
-impl Transform<Vec<u8>> for OhvLabel {
+impl OhvLabel {
+    pub const MISSING_LABEL: &'static str = "<MISSING>";
+
+    pub fn new() -> Self {
+        OhvLabel {
+            unknown_label: OhvLabel::MISSING_LABEL,
+            labels: Vec::new(),
+        }
+    }
+
+    pub fn with_unknown_label(self, unknown_label: &'static str) -> Self {
+        OhvLabel {
+            unknown_label,
+            ..self
+        }
+    }
+
+    pub fn with_labels<I>(self, labels: I) -> Self
+    where
+        I: IntoIterator<Item = &'static str>,
+    {
+        OhvLabel {
+            labels: labels.into_iter().collect(),
+            ..self
+        }
+    }
+}
+
+impl<S: AsRef<[u8]>> Transform<S> for OhvLabel {
     type Output = &'static str;
 
-    fn transform(&mut self, input: Vec<u8>) -> Self::Output {
+    fn transform(&mut self, input: S) -> Self::Output {
         input
+            .as_ref()
             .iter()
             .position(|&r| r == 1)
             .and_then(|index| self.labels.get(index))
@@ -28,60 +57,41 @@ impl Transform<Vec<u8>> for OhvLabel {
 
 #[cfg(test)]
 mod tests {
-    use rand::{thread_rng, Rng};
-
+    use super::*;
+    use alloc::vec;
     use runic_types::Transform;
-
-    use crate::OhvLabel;
 
     #[test]
     fn it_works() {
-        let mut input: [u8; 4] = [0; 4];
-        let mut rng = thread_rng();
-        let idx = rng.gen_range(0..4) as usize;
-        input[idx] = 1;
-
-        let mut pb = OhvLabel {
-            labels: vec!["Wing", "Ring", "Slope", "Unknown"],
-            unknown_label: "NO_LABEL_FOUND",
-        };
+        let input = [0, 1, 0, 0];
+        let mut pb = OhvLabel::new()
+            .with_unknown_label("NO_LABEL_FOUND")
+            .with_labels(vec!["Wing", "Ring", "Slope", "Unknown"]);
 
         let out = pb.transform(input.to_vec());
-        let labels = vec!["Wing", "Ring", "Slope", "Unknown"];
-        assert_eq!(out, labels[idx]);
-        println!("OhV={:?} | Label={}", input, out);
+
+        assert_eq!(out, "Ring");
     }
 
     #[test]
     fn handles_missing_labels() {
-        let mut input: [u8; 4] = [0; 4];
-        let mut rng = thread_rng();
-        let idx = rng.gen_range(0..4) as usize;
-        input[idx] = 1;
-
-        let mut pb = OhvLabel {
-            labels: vec![],
-            unknown_label: "NO_LABEL_FOUND",
-        };
+        let input = [0, 1];
+        let mut pb = OhvLabel::new().with_unknown_label("NO_LABEL_FOUND");
 
         let out = pb.transform(input.to_vec());
 
         assert_eq!(out, "NO_LABEL_FOUND");
-        println!("OhV={:?} | Label={}", input, out);
     }
 
     #[test]
     fn handles_null_ohv() {
-        let input: [u8; 4] = [0; 4];
+        let input = [0; 4];
+        let mut pb = OhvLabel::new()
+            .with_unknown_label("NO_LABEL_FOUND")
+            .with_labels(vec!["a", "b", "c", "d"]);
 
-        let mut pb = OhvLabel {
-            labels: vec!["a", "b", "c", "d"],
-            unknown_label: "NO_LABEL_FOUND",
-        };
-
-        let out = pb.transform(input.to_vec());
+        let out = pb.transform(input);
 
         assert_eq!(out, "NO_LABEL_FOUND");
-        println!("OhV={:?} | Label={}", input, out);
     }
 }
