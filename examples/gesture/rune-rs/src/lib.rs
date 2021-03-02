7d41571c04e5681a486ea17bc07b421382816425
Milind <milindsw6@gmail.com>
2021-03-02 10:31:34 -0500

    added predict function

--- examples/gesture/rune-rs/src/lib.rs
+++ examples/gesture/rune-rs/src/lib.rs
@@ -101,6 +101,43 @@ fn debug(s: &[u8]) -> u32 {
 
 }
 
+pub extern "C" fn PredictGesture(tfArray: [f32;64]) -> f32 {
+    unsafe{
+        float prediction_history[kGestureCount][kPredictionHistoryLength] = {};
+        int prediction_history_index = 0;
+        int prediction_suppression_count = 0;
+
+        for(int j=0; j < kgestureCount; j++){
+            prediction_history[i][prediction_history_index] = output[i];
+        }
+
+        prediction_history_index++;
+        if prediction_history_index >= kPredictionHistoryLength {
+            prediction_history_index = 0
+        }
+
+        int max_predict_index = -1;
+        float max_predict_score = 0.0;
+
+        for (int i = 0; i < kGestureCount; i++) {
+            float prediction_sum = 0.0;
+            for (int j = 0; j < kPredictionHistoryLength; ++j) {
+              prediction_sum += prediction_history[i][j];
+            }
+            const float prediction_average = prediction_sum / kPredictionHistoryLength;
+            if ((max_predict_index == -1) || (prediction_average > max_predict_score)) {
+              max_predict_index = i;
+              max_predict_score = prediction_average;
+            }
+        }
+
+        // If there's been a recent prediction, don't trigger a new one too soon.
+        if (prediction_suppression_count > 0) {
+            --prediction_suppression_count;
+        }
+    }
+}
+
 
 #[no_mangle]
 pub extern "C" fn _manifest() -> u32 {
