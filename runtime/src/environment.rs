6a1af52011f04eeae84153db8bb87ab62022d987
Michael-F-Bryan <michaelfbryan@gmail.com>
2021-03-04 21:18:09 +0800

    Switched to wasmer 1.0

--- runtime/src/environment.rs
+++ runtime/src/environment.rs
@@ -1,15 +1,24 @@
+use std::sync::Mutex;
+
 use log::Record;
 use rand::{rngs::SmallRng, Rng, RngCore, SeedableRng};
+use anyhow::Error;
 
-pub trait Environment: 'static {
-    fn rng(&mut self) -> Option<&mut dyn RngCore> { None }
+pub trait Environment: Send + Sync + 'static {
+    fn fill_random(&self, _buffer: &mut [u8]) -> Result<(), Error> {
+        Err(Error::new(NotSupportedError))
+    }
 
-    fn log(&mut self, _msg: &str) {}
+    fn log(&self, _msg: &str) {}
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Copy, Clone, PartialEq, thiserror::Error)]
+#[error("Not Supported")]
+pub struct NotSupportedError;
+
+#[derive(Debug)]
 pub struct DefaultEnvironment {
-    rng: SmallRng,
+    rng: Mutex<SmallRng>,
     name: String,
     accelerometer_samples: Vec<[f32; 3]>,
 }
@@ -24,15 +33,15 @@ impl DefaultEnvironment {
 
     pub fn with_seed(seed: [u8; 32]) -> Self {
         DefaultEnvironment {
-            rng: SmallRng::from_seed(seed),
+            rng: Mutex::new(SmallRng::from_seed(seed)),
             name: String::from("current_rune"),
             accelerometer_samples: Vec::new(),
         }
     }
 
-    /// Reset the [`Environment::rng()`]'s seed.
+    /// Reset the Random Number Generator's seed.
     pub fn seed_rng(&mut self, seed: u64) {
-        self.rng = SmallRng::seed_from_u64(seed);
+        self.rng = Mutex::new(SmallRng::seed_from_u64(seed));
     }
 
     pub fn set_accelerometer_data(&mut self, samples: Vec<[f32; 3]>) {
@@ -49,9 +58,13 @@ impl Default for DefaultEnvironment {
 }
 
 impl Environment for DefaultEnvironment {
-    fn rng(&mut self) -> Option<&mut dyn RngCore> { Some(&mut self.rng) }
+    fn fill_random(&self, buffer: &mut [u8]) -> Result<(), Error> {
+        self.rng.lock().unwrap().fill_bytes(buffer);
 
-    fn log(&mut self, msg: &str) {
+        Ok(())
+    }
+
+    fn log(&self, msg: &str) {
         // TODO: Update the _debug() function to take a file name and line
         // number.
         log::logger().log(
