af14b2e5fe00004ecb3621d159c62d2b82a479b6
Michael-F-Bryan <michaelfbryan@gmail.com>
2021-03-05 03:42:59 +0800

    Updated the names for some intrinsic function parameters

--- runic-types/src/wasm32/intrinsics.rs
+++ runic-types/src/wasm32/intrinsics.rs
@@ -10,12 +10,12 @@ extern "C" {
     ///
     /// Model failures will trigger a trap and abort at runtime.
     pub fn tfm_model_invoke(
-        feature_id: u32,
+        model_id: u32,
         input: *const u8,
         input_len: u32,
         output: *mut u8,
         output_len: u32,
-    ) -> i32;
+    ) -> u32;
 
     /// Load a model (as a byte buffer) into the runtime, telling it how many
     /// inputs and outputs there will be.
@@ -25,28 +25,28 @@ extern "C" {
     ///
     /// Model failures will trigger a trap and abort at runtime.
     pub fn tfm_preload_model(
-        model_idx: *const u8,
+        model: *const u8,
         model_len: u32,
         inputs: u32,
         outputs: u32,
     ) -> u32;
 
     /// Write some text to the debug console.
-    pub fn _debug(str_ptr: *const u8, str_len: u32) -> u32;
+    pub fn _debug(msg: *const u8, msg_len: u32) -> u32;
 
     /// Request a capability with a particular type, yielding a unique handle
     /// that can be used to refer to the capability later on.
     ///
     /// See [`crate::CAPABILITY`] to find out which capabilities are available.
-    pub fn request_capability(ct: u32) -> u32;
+    pub fn request_capability(capability_type: u32) -> u32;
 
     /// Set a capability parameter by name.
     ///
     /// Invalid parameters will trigger a trap and abort at runtime.
     pub fn request_capability_set_param(
-        idx: u32,
-        key_str_ptr: *const u8,
-        key_str_len: u32,
+        capability_id: u32,
+        key_ptr: *const u8,
+        key_len: u32,
         value_ptr: *const u8,
         value_len: u32,
         value_type: u32,
@@ -55,11 +55,14 @@ extern "C" {
     /// Direct the current pipeline's output to a particular location.
     ///
     /// See [`crate::OUTPUT`] to find out which outputs are available.
-    pub fn request_manifest_output(t: u32) -> u32;
+    pub fn request_manifest_output(out_type: u32) -> u32;
 
+    /// Ask a particular capability to fill the `buffer` with input.
+    ///
+    /// Invalid parameters will trigger a trap and abort at runtime.
     pub fn request_provider_response(
-        provider_response_idx: *const u8,
-        max_allowed_provider_response: u32,
-        capability_idx: u32,
+        buffer: *mut u8,
+        buffer_len: u32,
+        capability_id: u32,
     ) -> u32;
 }
