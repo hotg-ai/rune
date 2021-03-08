bdc5b2ac2bc10d4c83c364254817dad661aaa08b
Michael-F-Bryan <michaelfbryan@gmail.com>
2021-03-09 07:44:46 +0800

    Renamed the Audio builtin wrapper to Sound

--- runic-types/src/wasm32/sound.rs
+++ runic-types/src/wasm32/sound.rs
@@ -1,23 +1,23 @@
 use crate::{Source, wasm32::intrinsics, CAPABILITY, Buffer};
 
-pub struct Audio<const N: usize> {
+pub struct Sound<const N: usize> {
     index: u32,
 }
 
-impl<const N: usize> Audio<N> {
+impl<const N: usize> Sound<N> {
     pub fn new() -> Self {
         let index =
             unsafe { intrinsics::request_capability(CAPABILITY::SOUND as u32) };
 
-        Accelerometer { index }
+        Sound { index }
     }
 }
 
-impl<const N: usize> Default for Audio<N> {
-    fn default() -> Self { Audio::new() }
+impl<const N: usize> Default for Sound<N> {
+    fn default() -> Self { Sound::new() }
 }
 
-impl<const N: usize> Source for Audio<N> {
+impl<const N: usize> Source for Sound<N> {
     type Output = [i16; N];
 
     fn generate(&mut self) -> Self::Output {
