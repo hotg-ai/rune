5afe47943f2ee5fd27523477103e1560d203b87a
Michael-F-Bryan <michaelfbryan@gmail.com>
2021-03-10 02:29:57 +0800

    Added integration tests which use the microspeech test data

--- rune/tests/integration.rs
+++ rune/tests/integration.rs
@@ -11,6 +11,7 @@ fn example_dir() -> PathBuf {
 
 fn sine_dir() -> PathBuf { example_dir().join("sine") }
 fn gesture_dir() -> PathBuf { example_dir().join("gesture") }
+fn microspeech_dir() -> PathBuf { example_dir().join("microspeech") }
 
 #[test]
 fn compile_sine() {
@@ -80,4 +81,52 @@ fn run_gesture() {
         .success()
         .code(0)
         .stderr(predicates::str::is_empty().not());
+}
+
+#[test]
+fn identify_yes_microspeech() {
+    let dir = microspeech_dir();
+    let runefile = dir.join("Runefile");
+
+    let mut cmd = Command::cargo_bin("rune").unwrap();
+    cmd.arg("build").arg(&runefile).unwrap();
+
+    let rune = dir.join("microspeech.rune");
+
+    let mut cmd = Command::cargo_bin("rune").unwrap();
+    cmd.arg("run").arg(&rune).arg(format!(
+        "--capability=sound:{}",
+        dir.join("data").join("yes_01d22d03_nohash_0.wav").display()
+    ));
+
+    // FIXME: We should probably check the output for some well-known string
+    // indicating success.
+    cmd.assert()
+        .success()
+        .code(0)
+        .stderr(predicates::str::contains("Serial -> \"yes\""));
+}
+
+#[test]
+fn identify_no_microspeech() {
+    let dir = microspeech_dir();
+    let runefile = dir.join("Runefile");
+
+    let mut cmd = Command::cargo_bin("rune").unwrap();
+    cmd.arg("build").arg(&runefile).unwrap();
+
+    let rune = dir.join("microspeech.rune");
+
+    let mut cmd = Command::cargo_bin("rune").unwrap();
+    cmd.arg("run").arg(&rune).arg(format!(
+        "--capability=sound:{}",
+        dir.join("data").join("no_bf90a57a_nohash_1.wav").display()
+    ));
+
+    // FIXME: We should probably check the output for some well-known string
+    // indicating success.
+    cmd.assert()
+        .success()
+        .code(0)
+        .stderr(predicates::str::contains("Serial -> \"no\""));
 }
