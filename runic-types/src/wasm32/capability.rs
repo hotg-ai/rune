962ec32f60f80d023f7616b792de83556aa96ea0
Michael-F-Bryan <michaelfbryan@gmail.com>
2021-04-21 16:41:33 +0800

    Merged the capabilities and switched over to reading/writing tensors

---
+++ runic-types/src/wasm32/capability.rs
@@ -0,0 +1,78 @@
+use crate::{Tensor, Source, wasm32::intrinsics, Value};
+use core::marker::PhantomData;
+
+pub type Random<T> = GenericCapability<T, { crate::capabilities::RAND }>;
+pub type Accelerometer = GenericCapability<f32, { crate::capabilities::ACCEL }>;
+pub type Image = GenericCapability<u32, { crate::capabilities::IMAGE }>;
+pub type Sound = GenericCapability<i16, { crate::capabilities::SOUND }>;
+pub type Raw = GenericCapability<u8, { crate::capabilities::RAW }>;
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct GenericCapability<T, const KIND: u32> {
+    index: u32,
+    _type: PhantomData<fn() -> T>,
+}
+
+impl<T, const KIND: u32> GenericCapability<T, KIND> {
+    pub fn new() -> Self {
+        unsafe {
+            let index = intrinsics::request_capability(KIND);
+
+            GenericCapability {
+                index,
+                _type: PhantomData,
+            }
+        }
+    }
+}
+
+impl<T, const KIND: u32> Default for GenericCapability<T, KIND> {
+    fn default() -> Self { GenericCapability::new() }
+}
+
+impl<T: Default + Copy, const KIND: u32> Source for GenericCapability<T, KIND> {
+    type Output = Tensor<T>;
+
+    fn generate(&mut self) -> Self::Output {
+        let mut buffer = Tensor::zeroed(alloc::vec![0]);
+
+        let elements = buffer.make_elements_mut();
+        let byte_length = (elements.len() * core::mem::size_of::<T>()) as u32;
+
+        unsafe {
+            let response_size = intrinsics::request_provider_response(
+                elements.as_mut_ptr() as _,
+                byte_length,
+                self.index,
+            );
+
+            debug_assert_eq!(response_size, byte_length);
+        }
+
+        buffer
+    }
+
+    fn set_parameter(
+        &mut self,
+        key: &str,
+        value: impl Into<Value>,
+    ) -> &mut Self {
+        let value = value.into();
+
+        unsafe {
+            let mut buffer = Value::buffer();
+            let bytes_written = value.to_le_bytes(&mut buffer);
+
+            intrinsics::request_capability_set_param(
+                self.index,
+                key.as_ptr(),
+                key.len() as u32,
+                buffer.as_ptr(),
+                bytes_written as u32,
+                value.ty().into(),
+            );
+        }
+
+        self
+    }
+}
--- runic-types/src/wasm32/model.rs
+++ runic-types/src/wasm32/model.rs
@@ -1,4 +1,5 @@
-use crate::{wasm32::intrinsics, Transform, Buffer};
+use crate::{wasm32::intrinsics, Transform, Tensor};
+use alloc::vec::Vec;
 use core::marker::PhantomData;
 
 /// A machine learning model.
@@ -10,15 +11,15 @@ pub struct Model<Input, Output> {
     _type: PhantomData<fn(Input) -> Output>,
 }
 
-impl<In, Out, const M: usize, const N: usize> Model<[In; M], [Out; N]> {
+impl<In, Out> Model<In, Out> {
     /// Loads a model into the VM.
     pub fn load(raw_blob: &[u8]) -> Self {
         unsafe {
             let ix = intrinsics::tfm_preload_model(
                 raw_blob.as_ptr(),
                 raw_blob.len() as u32,
-                M as u32,
-                N as u32,
+                1,
+                1,
             );
 
             Model {
@@ -29,29 +30,29 @@ impl<In, Out, const M: usize, const N: usize> Model<[In; M], [Out; N]> {
     }
 }
 
-impl<In, Out> Transform<In> for Model<In, Out>
-where
-    Out: Buffer,
-    In: Buffer,
-{
-    type Output = Out;
+impl<In, Out: Default> Transform<Tensor<In>> for Model<In, Out> {
+    type Output = Tensor<Out>;
 
-    fn transform(&mut self, input: In) -> Out {
+    fn transform(&mut self, input: Tensor<In>) -> Tensor<Out> {
         unsafe {
-            let input_length = core::mem::size_of_val(&input);
+            let input_buffer = input.elements();
 
-            let mut output = Out::zeroed();
-            let output_length = core::mem::size_of_val(&output);
+            // FIXME: properly calculate the output dimensions
+            let dimensions = alloc::vec![0];
+
+            let len: usize = dimensions.iter().product();
+            let mut output_buffer = Vec::new();
+            output_buffer.resize_with(len, Out::default);
 
             let _ret = intrinsics::tfm_model_invoke(
                 self.index,
-                input.as_ptr() as *const u8,
-                input_length as u32,
-                output.as_mut_ptr() as *mut u8,
-                output_length as u32,
+                input_buffer.as_ptr() as *const u8,
+                core::mem::size_of_val(input_buffer) as u32,
+                output_buffer.as_mut_ptr() as *mut u8,
+                core::mem::size_of_val(output_buffer.as_slice()) as u32,
             );
 
-            output
+            Tensor::new_row_major(output_buffer.into(), dimensions)
         }
     }
 }
