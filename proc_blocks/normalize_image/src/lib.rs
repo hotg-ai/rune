2083447d7eb1cbfb856457f8b07c0c8b8bdd146a
Milind Sai Waghrey <milindsw6@gmail.com>
2021-05-19 16:39:36 -0400

    Can normalize 2d rgb image

--- proc_blocks/normalize_image/src/lib.rs
+++ proc_blocks/normalize_image/src/lib.rs
@@ -3,32 +3,40 @@
 extern crate alloc;
 use runic_types::{HasOutputs, Transform};
 
-#[derive(Debug, Clone, PartialEq)]
+const CHANNEL_SIZE: usize = 3;
 pub struct Normalize<const N: usize> {
-    values_as_float: [f32; N],
+    values_as_float: [[[f32; CHANNEL_SIZE]; N]; N],
 }
 
 impl<const N: usize> Normalize<N> {
     pub fn new() -> Self {
         Normalize {
-            values_as_float: [0.0; N],
+            values_as_float: [[[0.0; CHANNEL_SIZE]; N]; N],
         }
     }
 
-    fn one_to_one_normalizer(&mut self, input: [u8; N]) {
+    fn one_to_one_normalizer(&mut self, input: [[[u8; CHANNEL_SIZE]; N]; N]) {
         let scale: f32 = 2.0 / 255.0;
         let bias: f32 = -1.0;
 
-        for (i, value) in &mut input.iter().enumerate() {
-            self.values_as_float[i] = (*value as f32 * scale) + bias;
+        for (i, channel) in input.iter().enumerate() {
+            for (j, row) in channel.iter().enumerate() {
+                for (k, col) in row.iter().enumerate() {
+                    self.values_as_float[i][j][k] =
+                        (*col as f32 * scale) + bias;
+                }
+            }
         }
     }
 }
 
-impl<const N: usize> Transform<[u8; N]> for Normalize<N> {
-    type Output = [f32; N];
+impl<const N: usize> Transform<[[[u8; CHANNEL_SIZE]; N]; N]> for Normalize<N> {
+    type Output = [[[f32; CHANNEL_SIZE]; N]; N];
 
-    fn transform(&mut self, input: [u8; N]) -> Self::Output {
+    fn transform(
+        &mut self,
+        input: [[[u8; CHANNEL_SIZE]; N]; N],
+    ) -> Self::Output {
         self.one_to_one_normalizer(input);
         return self.values_as_float;
     }
@@ -47,11 +55,18 @@ mod tests {
 
     #[test]
     fn test_one_to_one() {
-        let mut pb: Normalize<3> = Normalize::new();
-        let input = [30, 56, 133];
+        let mut pb: Normalize<2> = Normalize::new();
+        let input =
+            [[[255, 0, 255], [0, 0, 0]], [[255, 255, 255], [30, 56, 133]]];
 
         let out = pb.transform(input);
 
-        assert_eq!(out, [-0.7647059, -0.5607843, 0.043137312]);
+        assert_eq!(
+            out,
+            [
+                [[1.0, -1.0, 1.0], [-1.0, -1.0, -1.0]],
+                [[1.0, 1.0, 1.0], [-0.7647059, -0.5607843, 0.043137312]]
+            ]
+        );
     }
 }
