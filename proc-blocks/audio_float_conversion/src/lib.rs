abc24cb66a397ad377ffce3c231d47d4e81a4d70
Milind Sai Waghrey <milindsw6@gmail.com>
2021-06-24 09:58:20 -0400

    cleaned up code, updated tests

--- proc-blocks/audio_float_conversion/src/lib.rs
+++ proc-blocks/audio_float_conversion/src/lib.rs
@@ -26,20 +26,6 @@ impl AudioFloatConversion {
             i16_max_as_float: I16_MAX_AS_FLOAT,
         }
     }
-
-    fn transform_inner(&mut self, input: Vec<i16>) -> [f32; 5] {
-        let mut recorded_vec: [f32; 5] = [0.0; 5];
-
-        // TODO: Need to fix i16::MIN being normalized to -1.0000305
-        // TODO: [96*64] should be [96,64]
-
-        for (i,i16_input) in input.iter().enumerate() {
-            recorded_vec[i] = *i16_input as f32 / self.i16_max_as_float;
-        }
-
-        recorded_vec
-    }
-
 }
 
 impl Default for AudioFloatConversion {
@@ -50,7 +36,7 @@ impl Transform<Tensor<i16>> for AudioFloatConversion {
     type Output = Tensor<f32>;
 
     fn transform(&mut self, input: Tensor<i16>) -> Self::Output {
-        input.map(|_dims, &value| (value as f32 / i16::MAX as f32).clamp(-1.0, 1.0)))
+        input.map(|_dims, &value| (value as f32 / i16::MAX as f32).clamp(-1.0, 1.0))
     }
 }
 
@@ -72,11 +58,11 @@ mod tests {
     #[test]
     fn handle_empty() {
         let mut pb = AudioFloatConversion::new();
-        let input = Tensor::new_vector(vec![0; 5]);
+        let input = Tensor::new_vector(vec![0; 15]);
 
         let got = pb.transform(input);
 
-        assert_eq!(got.dimensions(), &[5]);
+        assert_eq!(got.dimensions(), &[15]);
     }
 
     #[test]
@@ -85,10 +71,22 @@ mod tests {
         let min = i16::MIN;
 
         let mut pb = AudioFloatConversion::new();
-        let input = Tensor::new_vector(vec![0, max, min, min+1]);
+        let input = Tensor::new_vector(vec![0, max, min+1]);
 
         let got = pb.transform(input);
 
-        assert_eq!(got.elements()[0..4], [0.0, 1.0, -1.0000305, -1.0]);
+        assert_eq!(got.elements()[0..3], [0.0, 1.0, -1.0]);
+    }
+    #[test]
+    fn does_clutch_work() {
+        let max = i16::MAX;
+        let min = i16::MIN;
+
+        let mut pb = AudioFloatConversion::new();
+        let input = Tensor::new_vector(vec![max, min, min+1]);
+
+        let got = pb.transform(input);
+
+        assert_eq!(got.elements()[0..3], [1.0, -1.0, -1.0]);
     }
 }
