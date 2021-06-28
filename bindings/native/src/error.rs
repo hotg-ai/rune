7725235748d9468a4b07dd1b7a46c8e0833deca0
Michael-F-Bryan <michael@hotg.ai>
2021-06-28 21:16:07 +0800

    Started re-implementing native bindings using safer_ffi

---
+++ bindings/native/src/error.rs
@@ -0,0 +1,55 @@
+use std::{convert::TryInto, ops::Deref};
+#[allow(unused_imports)]
+use std::ops::Not;
+use safer_ffi::{boxed::Box, char_p::char_p_boxed, derive_ReprC, ffi_export};
+
+/// An error that may be returned from this library.
+#[derive_ReprC]
+#[ReprC::opaque]
+pub struct Error {
+    inner: anyhow::Error,
+}
+
+impl From<anyhow::Error> for Error {
+    fn from(inner: anyhow::Error) -> Self { Error { inner } }
+}
+
+impl Deref for Error {
+    type Target = anyhow::Error;
+
+    fn deref(&self) -> &Self::Target { &self.inner }
+}
+
+/// Free the error once you are done with it.
+#[ffi_export]
+pub fn rune_error_free(e: Box<Error>) { drop(e); }
+
+/// Return a newly allocated string containing the error's backtrace.
+#[ffi_export]
+pub fn rune_error_backtrace(error: &Error) -> char_p_boxed {
+    error
+        .backtrace()
+        .to_string()
+        .try_into()
+        .expect("Should never contain interior nulls")
+}
+
+/// Return a newly allocated string describing the error.
+#[ffi_export]
+pub fn rune_error_to_string(error: &Error) -> char_p_boxed {
+    format!("{}", error.inner)
+        .try_into()
+        .expect("Should never contain interior nulls")
+}
+
+/// Return a newly allocated string describing the error and any errors that
+/// may have caused it.
+///
+/// This will also contain a backtrace if the `RUST_BACKTRACE` environment
+/// variable is set.
+#[ffi_export]
+pub fn rune_error_to_string_verbose(error: &Error) -> char_p_boxed {
+    format!("{:?}", error.inner)
+        .try_into()
+        .expect("Should never contain interior nulls")
+}
--- bindings/native/src/lib.rs
+++ bindings/native/src/lib.rs
@@ -1,7 +1,30 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+//! Native bindings to the `rune` project.
+
+mod error;
+mod image;
+#[cfg(feature = "wasmer-runtime")]
+mod wasmer_runtime;
+
+/// The following test function is necessary for the header generation.
+#[safer_ffi::cfg_headers]
+#[test]
+fn generate_headers() -> Result<(), anyhow::Error> {
+    use std::path::Path;
+    use anyhow::Context;
+
+    let manifest_dir = Path::new(env!("CARGO_MANIFEST_DIR"));
+    let project_root = manifest_dir
+        .ancestors()
+        .find(|d| d.join(".git").exists())
+        .context("Unable to determine the project root")?;
+
+    let target_dir = project_root.join("target");
+    let header_file = target_dir.join("rune.h");
+
+    safer_ffi::headers::builder()
+        .with_guard("_RUST_RUNE_NATIVE_")
+        .to_file(&header_file)?
+        .generate()?;
+
+    Ok(())
 }
