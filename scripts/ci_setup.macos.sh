79fce7dc48c4e49a340aa3a5a09e84d8d32885fb
Michael-F-Bryan <michael@hotg.ai>
2021-07-06 09:42:36 +0800

    Switched from exec to source

--- scripts/ci_setup.macos.sh
+++ scripts/ci_setup.macos.sh
@@ -3,6 +3,8 @@
 # We need to update rustup because the mac version is out of date and
 # self-update is disabled. https://github.com/rust-lang/rustup/issues/2766
 curl https://sh.rustup.rs -sSf | sh -s -- --default-toolchain none -y
+# Make sure rust is on our path
+source ~/.cargo/env
 
 # and now we can do the linux setup like normal
-exec "$(git rev-parse --top-level)/ci_setup.linux.sh"
+source "$(git rev-parse --top-level)/ci_setup.linux.sh"
