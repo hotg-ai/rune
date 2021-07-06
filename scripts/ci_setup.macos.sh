447ec7d2eece5a7d4043711a202dcbdb273b00d8
Michael-F-Bryan <michael@hotg.ai>
2021-07-06 10:22:34 +0800

    Typo

--- scripts/ci_setup.macos.sh
+++ scripts/ci_setup.macos.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+set -xe
+
 # We need to update rustup because the mac version is out of date and
 # self-update is disabled. https://github.com/rust-lang/rustup/issues/2766
 curl https://sh.rustup.rs -sSf | sh -s -- --default-toolchain none -y
@@ -7,4 +9,4 @@ curl https://sh.rustup.rs -sSf | sh -s -- --default-toolchain none -y
 source ~/.cargo/env
 
 # and now we can do the linux setup like normal
-exec "$(git rev-parse --show-toplevel)/ci_setup.linux.sh"
+exec "$(git rev-parse --show-toplevel)/scripts/ci_setup.linux.sh"
