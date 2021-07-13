280b6e4bca21448dad79ed8efacb5500422cef0e
Michael-F-Bryan <michael@hotg.ai>
2021-07-14 02:31:08 +0800

    Tweaking the Dockerfile and runner scripts

--- util/build.sh
+++ util/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -xe
 docker build . -t tinyverseml/rune-cli:latest -t tinyverseml/rune-cli:$(git rev-parse --short HEAD)
-docker push tinyverseml/rune-cli:latest
+docker push tinyverseml/rune-cli --all-tags
