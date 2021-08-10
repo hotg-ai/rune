82d3c30e6d93026dbc8729658a015b1ad67da3b3
Michael-F-Bryan <michael@hotg.ai>
2021-08-11 06:12:50 +0800

    Copied across the JavaScript code

---
+++ bindings/web/src/constants.ts
@@ -0,0 +1,10 @@
+export const KnownCapabilities = {
+    1: "rand",
+    2: "sound",
+    3: "accel",
+    4: "image",
+    5: "raw",
+};
+export const KnownOutputs = {
+    1: "serial",
+};
---
+++ bindings/web/src/index.ts
@@ -0,0 +1,2 @@
+export * from "./constants.js";
+export { default as Runtime } from "./Runtime.js";
