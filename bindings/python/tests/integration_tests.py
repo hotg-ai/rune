a41dbd578c36a43457b3021ca574183e09fb5c94
Michael-F-Bryan <michael@hotg.ai>
2021-10-11 04:16:10 +0800

    Python modules use __author__

--- bindings/python/tests/integration_tests.py
+++ bindings/python/tests/integration_tests.py
@@ -6,4 +6,4 @@ import numpy as np
 
 class SmokeTest(unittest.TestCase):
     def test_it_works(self):
-        assert rune_py.__AUTHORS__ == "The Rune Developers <developers@hotg.ai>"
+        assert rune_py.__authors__ == "The Rune Developers <developers@hotg.ai>"
