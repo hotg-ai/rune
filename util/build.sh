725bce1fffbae501d6b24827eb93af37de5f97d0
Michael-F-Bryan <michael@hotg.ai>
2021-11-02 00:26:23 +0800

    Deleted some unused scripts

--- util/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-set -xe
-docker build . -t tinyverseml/rune-cli:latest -t tinyverseml/rune-cli:$(git rev-parse --short HEAD)
-docker push tinyverseml/rune-cli --all-tags
--- util/rune.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-exec docker run -v `pwd`:`pwd` -w `pwd`  -i -t tinyverseml/rune-cli $@
