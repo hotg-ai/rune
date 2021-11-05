e74052634b4432c34e2541d1bb617f0a1fbb95a4
Michael-F-Bryan <michael@hotg.ai>
2021-11-06 06:17:11 +0800

    Made the web runtime tests pass again

--- bindings/web/src/Runtime.test.ts
+++ bindings/web/src/Runtime.test.ts
@@ -12,29 +12,60 @@ describe("Runtime", () => {
     const noopRune = buildExample("noop");
 
     it("can load the noop Rune", async () => {
-        const calls: Uint8Array[] = [];
         const imports = {
             createCapability: () => new RawCapability(),
-            createOutput: () => new SpyOutput(calls),
+            createOutput: () => new SpyOutput([]),
             createModel: () => { throw new Error(); },
-            log: () => { throw new Error(); },
+            log: (msg: any) => { },
         };
 
         const runtime = await Runtime.load(noopRune, imports);
 
         expect(runtime).not.toBeNull();
+    });
+
+    it("can run the noop Rune", async () => {
+        const calls: Uint8Array[] = [];
+        const imports = {
+            createCapability: () => new RawCapability([
+                1, 0, 0, 0,
+                2, 0, 0, 0,
+                3, 0, 0, 0,
+                4, 0, 0, 0,
+            ]),
+            createOutput: () => new SpyOutput(calls),
+            createModel: () => { throw new Error(); },
+            log: (msg: any) => { },
+        };
+        const runtime = await Runtime.load(noopRune, imports);
+
+        runtime.call();
+
         expect(calls).toHaveLength(1);
         const output = decoder.decode(calls[0]);
-        expect(JSON.parse(output)).toEqual({ asd: "TODO" });
+        expect(JSON.parse(output)).toEqual({
+            channel: 1,
+            dimensions: [4,],
+            elements: [1, 2, 3, 4,],
+            type_name: "i32",
+        });
     });
 });
 
 class RawCapability implements Capability {
+    data: Uint8Array = new Uint8Array();
+
+    constructor(data?: number[]) {
+        if (data) {
+            this.data = Uint8Array.from(data);
+        }
+    }
+
     setParameter(name: string, value: number): void {
         throw new Error("Method not implemented.");
     }
     generate(dest: Uint8Array): void {
-        throw new Error("Method not implemented.");
+        dest.set(this.data);
     }
 }
 
@@ -56,7 +87,7 @@ function buildExample(name: string): ArrayBuffer {
     const exampleDir = path.join(repoRoot, "examples", name);
     const runefile = path.join(exampleDir, "Runefile.yml");
 
-    child_process.execSync(`cargo rune build ${runefile} --quiet --unstable --rune-repo-dir ${repoRoot}`, {
+    child_process.execSync(`cargo run --bin rune --quiet -- build ${runefile} --quiet --unstable --rune-repo-dir ${repoRoot}`, {
         cwd: repoRoot,
         env: {
             RUST_LOG: "warning",
