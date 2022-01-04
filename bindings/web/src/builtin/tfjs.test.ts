2ab376f6ff381936185110ff9ff8a2272c7c6fa1
Michael-F-Bryan <michael@hotg.ai>
2022-01-04 13:04:12 +0800

    Added a test to make sure floats are inferred correctly

--- bindings/web/src/builtin/tfjs.test.ts
+++ bindings/web/src/builtin/tfjs.test.ts
@@ -10,6 +10,15 @@ describe("TensorFlowModel", () => {
 
         expect(model.inputs).toEqual([new Shape("float32", [1])]);
         expect(model.outputs).toEqual([new Shape("float32", [1])]);
+
+        // According to our training notebook, sine(3.0) = 0.1255441
+        const input = new Float32Array([3.0]);
+        const inputs = [new Uint8Array(input.buffer)];
+        const output = new Float32Array(1);
+        const outputs = [new Uint8Array(output.buffer)];
+        const shape = [Shape.parse("f32[1]")];
+        model.transform(inputs, shape, outputs, shape);
+        expect(output[0]).toBeCloseTo(0.1255441);
     });
 
     it("can load a tf.js graph model", async () => {
