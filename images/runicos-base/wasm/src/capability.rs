a515ed9d2b9412ca8f545e940161d92b40bd2b51
Michael-F-Bryan <michael@hotg.ai>
2022-01-12 14:47:53 +0800

    Make sure you can use strings when setting a capability's parameter

--- images/runicos-base/wasm/src/capability.rs
+++ images/runicos-base/wasm/src/capability.rs
@@ -43,12 +43,17 @@ impl<T: Clone + Default> Capability<T> {
         buffer
     }
 
-    pub fn set_parameter(
-        &mut self,
-        key: &str,
-        value: impl Into<Value>,
-    ) -> &mut Self {
-        let value = value.into();
+    pub fn set_parameter(&mut self, key: &str, value: &str) -> &mut Self {
+        let value = if let Ok(int) = value.parse() {
+            Value::Integer(int)
+        } else if let Ok(float) = value.parse() {
+            Value::Float(float)
+        } else {
+            panic!(
+                "Unable to parse \"{}\" as a number when setting \"{}\"",
+                value, key
+            );
+        };
 
         unsafe {
             let mut buffer = Value::buffer();
