ae9732743da7fcebcef278968069c38b75741fd3
Michael-F-Bryan <michael@hotg.ai>
2022-01-24 09:06:28 +0800

    Removed the Python package

--- bindings/python/tests/integration_tests.py
+++
@@ -1,9 +0,0 @@
-import unittest
-import math
-import rune_py
-import numpy as np
-
-
-class SmokeTest(unittest.TestCase):
-    def test_it_works(self):
-        assert rune_py.__authors__ == "The Rune Developers <developers@hotg.ai>"
