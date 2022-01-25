e770dd2897ed70ea2003056ef74b15309949c2c5
Michael-F-Bryan <michael@hotg.ai>
2022-01-25 15:57:51 +0800

    Export backend functions instead of constants

--- bindings/web/tfjs-v3/src/index.ts
+++ bindings/web/tfjs-v3/src/index.ts
@@ -3,7 +3,9 @@ export { loadTensorFlowJS } from "./load";
 import { loadTensorFlowJS } from "./load";
 import { mimetypes } from "@hotg-ai/rune/builtin";
 
-export default {
-    mimetype: mimetypes.tfjs,
-    load: loadTensorFlowJS,
-} as const;
+export default function backend() {
+    return {
+        mimetype: mimetypes.tfjs,
+        load: loadTensorFlowJS,
+    } as const;
+}
