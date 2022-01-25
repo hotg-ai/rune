4549e618701d162dd4afe8185fa122030fb5a801
Michael-F-Bryan <michael@hotg.ai>
2022-01-25 12:25:46 +0800

    Export inference  backend objects

--- bindings/web/tfjs-v3/src/index.ts
+++ bindings/web/tfjs-v3/src/index.ts
@@ -1,1 +1,9 @@
 export { loadTensorFlowJS } from "./load";
+
+import { loadTensorFlowJS } from "./load";
+import { mimetypes } from "@hotg-ai/rune/builtin";
+
+export default {
+    mimetype: mimetypes.tfjs,
+    load: loadTensorFlowJS,
+} as const;
