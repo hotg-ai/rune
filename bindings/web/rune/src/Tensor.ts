f82c8fc39ec1b3ca406f9aeb320185af8045f14c
Michael-F-Bryan <michael@hotg.ai>
2022-02-09 04:17:40 +0800

    Documented when calling tensor.asTypedArray("u64") may fail

--- bindings/web/rune/src/Tensor.ts
+++ bindings/web/rune/src/Tensor.ts
@@ -21,8 +21,17 @@ const typedArrays = {
     "i8": Int8Array,
 } as const;
 
+/**
+ * An opaque tensor.
+ */
 export default class Tensor {
+    /**
+     * The raw bytes containing the tensor data.
+     */
     public readonly elements: Uint8Array;
+    /**
+     * The tensor's shape (element type and dimensions).
+     */
     public readonly shape: Shape;
 
     constructor(shape: Shape, elements: Uint8Array) {
@@ -30,16 +39,68 @@ export default class Tensor {
         this.elements = elements;
     }
 
+    /**
+     * View this tensor's data as an array of 64-bit floats.
+     *
+     * This will fail if this isn't a f64 tensor.
+     */
     public asTypedArray(elementType: "f64"): Float64Array;
+    /**
+     * View this tensor's data as an array of 64-bit signed integers.
+     *
+     * This will fail if this isn't a i64 tensor. It may also fail on
+     * versions of Safari because they don't support BigInt64Array.
+     */
     public asTypedArray(elementType: "i64"): BigInt64Array;
+    /**
+     * View this tensor's data as an array of 64-bit unsigned integers.
+     *
+     * This will fail if this isn't a u64 tensor. It may also fail on
+     * versions of Safari because they don't support BigUint64Array.
+     */
     public asTypedArray(elementType: "u64"): BigUint64Array;
+    /**
+     * View this tensor's data as an array of 32-bit floats.
+     *
+     * This will fail if this isn't a f32 tensor.
+     */
     public asTypedArray(elementType: "f32"): Float32Array;
+    /**
+     * View this tensor's data as an array of 32-bit signed integers.
+     *
+     * This will fail if this isn't a i32 tensor.
+     */
     public asTypedArray(elementType: "i32"): Int32Array;
+    /**
+     * View this tensor's data as an array of 32-bit unsigned integers.
+     *
+     * This will fail if this isn't a u32 tensor.
+     */
     public asTypedArray(elementType: "u32"): Uint32Array;
-    public asTypedArray(elementType: "u16"): Uint16Array;
+    /**
+     * View this tensor's data as an array of 16-bit signed integers.
+     *
+     * This will fail if this isn't a i16 tensor.
+     */
     public asTypedArray(elementType: "i16"): Int16Array;
-    public asTypedArray(elementType: "u8"): Uint8ClampedArray;
+    /**
+     * View this tensor's data as an array of 16-bit unsigned integers.
+     *
+     * This will fail if this isn't a u16 tensor.
+     */
+    public asTypedArray(elementType: "u16"): Uint16Array;
+    /**
+     * View this tensor's data as an array of 8-bit signed integers.
+     *
+     * This will fail if this isn't a i8 tensor.
+     */
     public asTypedArray(elementType: "i8"): Int8Array;
+    /**
+     * View this tensor's data as an array of 8-bit unsigned integers.
+     *
+     * This will fail if this isn't a u8 tensor.
+     */
+    public asTypedArray(elementType: "u8"): Uint8ClampedArray;
 
     public asTypedArray(elementType: keyof typeof typedArrays): ArrayBuffer {
         if (this.shape.type != elementType) {
