08afed086a48781effa27d5f11d02969df9e89f6
Dinesh Manajipet <saidinesh5@gmail.com>
2022-05-20 20:13:40 +0530

    Zune update example

--- crates/runtime/examples/zune_example.rs
+++ crates/runtime/examples/zune_example.rs
@@ -1,4 +1,4 @@
-use hotg_rune_runtime::zune::ZuneEngine;
+use hotg_rune_runtime::zune::{ZuneEngine, TensorResult, ElementType};
 
 fn main() {
     println!("Hello World!");
@@ -9,14 +9,19 @@ fn main() {
     println!("input nodes {:?}",  zune_engine.input_nodes());
     println!("output nodes {:?}",  zune_engine.output_nodes());
     println!("input tensor names of rand => {:?}", zune_engine.get_input_tensor_names("rand"));
+    println!("output tensor names of sine => {:?}", zune_engine.get_output_tensor_names("sine"));
 
-    // let input_tensor = TensorResult {
-    //     element_type: ElementType::f32,
-    //     shape: [1,1],
-    //     buffer: vec![ 0 as u8, 0, 0, 0]
-    // };
+    let input_tensor = TensorResult {
+        element_type: ElementType::F32,
+        dimensions: vec![1,1],
+        buffer: vec![ 0, 0, 0, 0]
+    };
+
+    zune_engine.set_input_tensor("rand", "input", &input_tensor);
 
     println!("input tensor rand => {:?}", zune_engine.get_input_tensor("rand", "input"));
 
     zune_engine.predict().expect("Failed to run predict!");
+
+    println!("input tensor for serial: => {:?}", zune_engine.get_input_tensor("serial", "input"));
 }
